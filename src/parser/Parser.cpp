36dad19fc422be2a2929bf562c45de9ab315f42b
ymimsr <43496855+ymimsr@users.noreply.github.com>
2020-01-28 15:51:00 +0700

    fixed mistakes
    added skipToken
    removed index

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -7,45 +7,28 @@
 #include <iostream>
 
 using namespace std;
-int index = 0;
 IR parseProgram(vector<Token> tokens) {
-    for (int i = 0; i < tokens.size(); i++) {
-        if (tokens[i].type == TT_KEYWORD) {
-            if (tokens[i].value == 1) {
-                parseFunction(tokens);
-            }
-        }
-    }
+    parseFunction(tokens);
 }
 
 Function parseFunction(vector<Token> tokens) {
-    Function *function = new Function();
+    Function* function = new Function();
     string name;
 
-    while (index < tokens.size()) {
-        readFunctionType(tokens);
-        readFunctionName(tokens);
+    readFunctionType(tokens);
+    readFunctionName(tokens);
 
-        Token openParenthesis = tokens.at(index);
-        if (openParenthesis.type != TT_OPERATION || openParenthesis.value != OPEN_PARENTHESE) { /* throw an exception */ }
-        index++;
+    skipToken(TT_OPERATION, OPEN_PARENTHESE, tokens);
 
-        // readParameters(tokens);
+    // readParameters(tokens);
 
-        Token closeParenthesis = tokens.at(index);
-        if (closeParenthesis.type != TT_OPERATION || closeParenthesis.value != CLOSE_PARENTHESE) { /* throw an exception */ }
-        index++;
+    skipToken(TT_OPERATION, CLOSE_PARENTHESE, tokens);
 
-        Token openBrace = tokens.at(index);
-        if (openBrace.type != TT_OPERATION || openBrace.value != OPEN_BRACE) { /* throw an exception */ }
-        index++;
+    skipToken(TT_OPERATION, OPEN_BRACE, tokens);
 
-        //parseBody(tokens);
+    //parseBody(tokens);
 
-        Token closeBrace = tokens.at(index);
-        if (closeBrace.type != TT_OPERATION || closeBrace.value != CLOSE_BRACE) { /* throw an exception */ }
-        index++;
-    }
+    skipToken(TT_OPERATION, CLOSE_BRACE, tokens);
 
     return *function;
 }
@@ -53,25 +36,27 @@ Function parseFunction(vector<Token> tokens) {
 vector<Operator*> parseBody(vector<Token> tokens) {
     vector<Operator*> body;
 
-    while (index < tokens.size()) {
+    skipToken(TT_OPERATION, OPEN_BRACE, tokens);
 
+    while (tokens.front().type != TT_OPERATION && tokens.front().value != CLOSE_BRACE) {
+        body.push_back(parseOperator(tokens));
     }
+
+    skipToken(TT_OPERATION, CLOSE_BRACE, tokens);
 }
 
 string readFunctionType(vector<Token> tokens) {
     string returnType;
 
-    Token returnTypeToken = tokens.at(index);
-    if (returnTypeToken.type != TT_KEYWORD) { /* throw an exception */ }
+    Token returnTypeToken = getToken(tokens);
+    if (returnTypeToken.type != TT_KEYWORD) { throw; }
     switch (returnTypeToken.value) {
         case KW_VOID :
             returnType = "void";
             break;
         default:;
-            // throw an exception
+            throw;
     }
-
-    index++;
 
     return returnType;
 }
@@ -79,32 +64,83 @@ string readFunctionType(vector<Token> tokens) {
 string readFunctionName(vector<Token> tokens) {
     string name;
 
-    Token functionNameToken = tokens.at(index);
-    if (functionNameToken.type != TT_IDENT) { /* throw an exception */ }
-    name = functionNameToken.value;
+    Token functionNameToken = getToken(tokens);
+    if (functionNameToken.type != TT_IDENT) {
+        throw;
+    }
 
-    index++;
+    name = functionNameToken.value;
 
     return name;
 }
 
-Operator readOperator(vector<Token> tokens) {
-    Token operatorToken = tokens.at(index);
+Operator* parseOperator(vector<Token> tokens) {
+    Token operatorToken = tokens.front();
     switch (operatorToken.type) {
         case TT_KEYWORD :
             switch(operatorToken.value) {
-                case KW_IF :
-                    
+                case KW_IF : {
+                    skipToken(TT_OPERATION, OPEN_PARENTHESE, tokens);
+
+                    Expression ifCondition = parseExpression(tokens);
+
+                    skipToken(TT_OPERATION, CLOSE_PARENTHESE, tokens);
+
+                    vector<Operator*> ifBody = parseBody(tokens);
+                    }
                     break;
                 case KW_WHILE :
                     break;
-                case KW_INT:
+                case KW_INT: {
+                    VarDefOperator* defOperator = new VarDefOperator();
+
+                    Token nameToken = getToken(tokens);
+                    if (nameToken.type != TT_IDENT) {
+                        throw;
+                    }
+
+                    defOperator->name = nameToken.value;
+
+                    skipToken(TT_OPERATION, SEMICOLON, tokens);
+
+                    return defOperator;
+                    }
                     break;
-                case KW_RETURN:
+                case KW_RETURN: {
+                    ExpressionOperator *returnOperator = new ExpressionOperator();
+
+                    }
                     break;
-                default:;
+                default:
+                    break;
                     //throw an exception
             }
             break;
+        case TT_IDENT :
+            break;
+        case TT_LITERAL:
+            break;
+        case TT_OPERATION:
+            break;
     }
+}
+
+Expression parseExpression(vector<Token> tokens) {
+
+}
+
+void skipToken(TokenType tokenType, int ttType, vector<Token> tokens) {
+    auto index = tokens.cbegin();
+    if (tokens.at(0).type == tokenType && tokens.at(0).value == ttType) {
+        tokens.erase(index);
+    } else {
+        throw;
+    }
+}
+
+Token getToken(vector<Token> tokens) {
+    Token token = tokens.front();
+    tokens.erase(tokens.cbegin());
+
+    return token;
 }
--- src/parser/Parser.h
+++ src/parser/Parser.h
@@ -1,6 +1,7 @@
 //
 // Created by salah on 29.10.2019.
 //
+#pragma once
 #include "../base.h"
 #ifndef LANG_2019_PARSER_H
 #define LANG_2019_PARSER_H
@@ -9,4 +10,8 @@ IR parseProgram(std::vector<Token> tokens);
 Function parseFunction(std::vector<Token> tokens);
 std::string readFunctionType(std::vector<Token> tokens);
 std::string readFunctionName(std::vector<Token> tokens);
+Expression parseExpression(std::vector<Token> tokens);
+void skipToken(TokenType tokenType, int ttType, std::vector<Token> tokens);
+Operator* parseOperator(std::vector<Token> tokens);
+Token getToken(std::vector<Token> tokens);
 #endif //LANG_2019_PARSER_H
