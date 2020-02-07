5d00a3e3bf23fbe30e37ebfd69e0c2dd79052df1
ymimsr <43496855+ymimsr@users.noreply.github.com>
2020-02-07 19:17:21 +0700

    fixed mistakes 3
    fixed Parser.h

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -9,51 +9,46 @@
 
 using namespace std;
 
+bool nextToken(TokenType tokenType, int ttType, stack<Token> stack) {
+    return stack.top().type == tokenType && stack.top().value == ttType;
+}
+
+bool nextToken(TokenType tokenType, int ttType, vector<Token> tokens) {
+    return tokens.at(0).type == tokenType && tokens.at(0).value == ttType;
+}
+
+bool nextToken(TokenType tokenType, vector<Token> tokens) {
+    return tokens.at(0).type == tokenType;
+}
+
+void skipToken(TokenType tokenType, int ttType, vector<Token> tokens) {
+    auto index = tokens.begin();
+    if (tokens.at(0).type == tokenType && tokens.at(0).value == ttType) {
+        tokens.erase(index);
+    } else {
+        throw;
+    }
+}
+
+Token getToken(vector<Token> tokens) {
+    Token token = tokens.at(0);
+    tokens.erase(tokens.begin());
+
+    return token;
+}
+
+Token getToken(stack<Token> stack) {
+    Token token = stack.top();
+    stack.pop();
+
+    return token;
+}
+
 Expression* getExpression(stack<Expression*> stack) {
     Expression* expression = stack.top();
     stack.pop();
 
     return expression;
-}
-
-IR parseProgram(vector<Token> tokens) {
-    while (!tokens.empty()) {
-        parseFunction(tokens);
-    }
-}
-
-Function* parseFunction(vector<Token> tokens) {
-    Function* function = new Function();
-    int name;
-
-    readFunctionType(tokens);
-    name = readFunctionName(tokens);
-
-    skipToken(TT_OPERATION, OPEN_PARENTHESE, tokens);
-
-    //readParameters(tokens);
-
-    skipToken(TT_OPERATION, CLOSE_PARENTHESE, tokens);
-
-    vector<Operator*> body = parseBody(tokens);
-
-    function->name = name;
-    function->body = body;
-    if (name == -1) function->isMain = true; // main number
-
-    return function;
-}
-
-vector<Operator*> parseBody(vector<Token> tokens) {
-    vector<Operator*> body;
-
-    skipToken(TT_OPERATION, OPEN_BRACE, tokens);
-
-    while (nextToken(TT_OPERATION, CLOSE_BRACE, tokens)) {
-        body.push_back(parseOperator(tokens));
-    }
-
-    skipToken(TT_OPERATION, CLOSE_BRACE, tokens);
 }
 
 string readFunctionType(vector<Token> tokens) {
@@ -73,85 +68,25 @@ int readFunctionName(vector<Token> tokens) {
     throw;
 }
 
-Operator* parseOperator(vector<Token> tokens) {
-    Token operatorToken = tokens.front();
-    switch (operatorToken.type) {
-        case TT_KEYWORD :
-            switch(operatorToken.value) {
-                case KW_IF : {
-                    skipToken(TT_KEYWORD, KW_IF, tokens);
-
-                    IfOperator* ifOperator = new IfOperator();
-
-                    skipToken(TT_OPERATION, OPEN_PARENTHESE, tokens);
-
-                    Expression* ifCondition = parseExpression(tokens);
-                    ifOperator->condition = ifCondition;
-
-                    skipToken(TT_OPERATION, CLOSE_PARENTHESE, tokens);
-
-                    vector<Operator*> thenBody = parseBody(tokens);
-                    ifOperator->thenPart = thenBody;
-
-                    if (nextToken(TT_KEYWORD, KW_ELSE, tokens)) {
-                        skipToken(TT_KEYWORD, KW_ELSE, tokens);
-                        vector<Operator*> elseBody = parseBody(tokens);
-                        ifOperator->elsePart = elseBody;
-                    }
-
-                    return ifOperator;
-                    }
-                case KW_WHILE : {
-                    skipToken(TT_KEYWORD, KW_WHILE, tokens);
-
-                    WhileOperator* whileOperator = new WhileOperator();
-
-                    skipToken(TT_OPERATION, OPEN_PARENTHESE, tokens);
-
-                    Expression* whileCondition = parseExpression(tokens);
-                    whileOperator->condition = whileCondition;
-
-                    skipToken(TT_OPERATION, CLOSE_PARENTHESE, tokens);
-
-                    vector<Operator*> whileBody = parseBody(tokens);
-                    whileOperator->body = whileBody;
-
-                    return whileOperator;
-                    }
-                case KW_INT: {
-                    skipToken(TT_KEYWORD, KW_INT, tokens);
-
-                    VarDefOperator* defOperator = new VarDefOperator();
-
-                    Token nameToken = getToken(tokens);
-                    if (nameToken.type != TT_IDENT) {
-                        throw;
-                    }
-
-                    defOperator->name = nameToken.value;
-
-                    skipToken(TT_OPERATION, SEMICOLON, tokens);
-
-                    return defOperator;
-                    }
-                case KW_RETURN: {
-                    ExpressionOperator* returnOperator = new ExpressionOperator();
-                    }
-                    break;
-                default:
-                    throw;
-            }
-            break;
-        case TT_IDENT :
-        case TT_LITERAL:
-        case TT_OPERATION: {
-            ExpressionOperator* expressionOperator = new ExpressionOperator();
-
-            Expression* expression = parseExpression(tokens);
-            expressionOperator->expr = expression;
-
-            return expressionOperator;
-            }
+int getPriority(Token token) {
+    switch (token.value) {
+        case EQ:
+        case NE:
+        case LS:
+        case BG:
+        case LS_EQ:
+        case BG_EQ:
+        case AND:
+        case OR:
+        case NOT:
+            return 1;
+        case ADD:
+        case SUB:
+        case MOD:
+            return 2;
+        case MUL:
+        case DIV:
+            return 3;
     }
 }
 
@@ -204,59 +139,126 @@ Expression* parseExpression(vector<Token> tokens) {
     return expressionStack.top();
 }
 
-int getPriority(Token token) {
-    switch (token.value) {
-        case EQ:
-        case NE:
-        case LS:
-        case BG:
-        case LS_EQ:
-        case BG_EQ:
-        case AND:
-        case OR:
-        case NOT:
-            return 1;
-        case ADD:
-        case SUB:
-        case MOD:
-            return 2;
-        case MUL:
-        case DIV:
-            return 3;
+vector<Operator*> parseBody(vector<Token> tokens);
+
+Operator* parseOperator(vector<Token> tokens) {
+    Token operatorToken = tokens.front();
+    switch (operatorToken.type) {
+        case TT_KEYWORD :
+            switch(operatorToken.value) {
+                case KW_IF : {
+                    skipToken(TT_KEYWORD, KW_IF, tokens);
+
+                    IfOperator* ifOperator = new IfOperator();
+
+                    skipToken(TT_OPERATION, OPEN_PARENTHESE, tokens);
+
+                    Expression* ifCondition = parseExpression(tokens);
+                    ifOperator->condition = ifCondition;
+
+                    skipToken(TT_OPERATION, CLOSE_PARENTHESE, tokens);
+
+                    vector<Operator*> thenBody = parseBody(tokens);
+                    ifOperator->thenPart = thenBody;
+
+                    if (nextToken(TT_KEYWORD, KW_ELSE, tokens)) {
+                        skipToken(TT_KEYWORD, KW_ELSE, tokens);
+                        vector<Operator*> elseBody = parseBody(tokens);
+                        ifOperator->elsePart = elseBody;
+                    }
+
+                    return ifOperator;
+                }
+                case KW_WHILE : {
+                    skipToken(TT_KEYWORD, KW_WHILE, tokens);
+
+                    WhileOperator* whileOperator = new WhileOperator();
+
+                    skipToken(TT_OPERATION, OPEN_PARENTHESE, tokens);
+
+                    Expression* whileCondition = parseExpression(tokens);
+                    whileOperator->condition = whileCondition;
+
+                    skipToken(TT_OPERATION, CLOSE_PARENTHESE, tokens);
+
+                    vector<Operator*> whileBody = parseBody(tokens);
+                    whileOperator->body = whileBody;
+
+                    return whileOperator;
+                }
+                case KW_INT: {
+                    skipToken(TT_KEYWORD, KW_INT, tokens);
+
+                    VarDefOperator* defOperator = new VarDefOperator();
+
+                    Token nameToken = getToken(tokens);
+                    if (nameToken.type != TT_IDENT) {
+                        throw;
+                    }
+
+                    defOperator->name = nameToken.value;
+
+                    skipToken(TT_OPERATION, SEMICOLON, tokens);
+
+                    return defOperator;
+                }
+                case KW_RETURN: {
+                    ExpressionOperator* returnOperator = new ExpressionOperator();
+                }
+                    break;
+                default:
+                    throw;
+            }
+            break;
+        case TT_IDENT:
+        case TT_LITERAL:
+        case TT_OPERATION: {
+            ExpressionOperator* expressionOperator = new ExpressionOperator();
+
+            Expression* expression = parseExpression(tokens);
+            expressionOperator->expr = expression;
+
+            return expressionOperator;
+        }
     }
 }
 
-bool nextToken(TokenType tokenType, int ttType, stack<Token> stack) {
-    return stack.top().type == tokenType && stack.top().value == ttType;
-}
+vector<Operator*> parseBody(vector<Token> tokens) {
+    vector<Operator*> body;
 
-bool nextToken(TokenType tokenType, int ttType, vector<Token> tokens) {
-    return tokens.at(0).type == tokenType && tokens.at(0).value == ttType;
-}
+    skipToken(TT_OPERATION, OPEN_BRACE, tokens);
 
-bool nextToken(TokenType tokenType, vector<Token> tokens) {
-    return tokens.at(0).type == tokenType;
-}
-
-void skipToken(TokenType tokenType, int ttType, vector<Token> tokens) {
-    auto index = tokens.begin();
-    if (tokens.at(0).type == tokenType && tokens.at(0).value == ttType) {
-        tokens.erase(index);
-    } else {
-        throw;
+    while (nextToken(TT_OPERATION, CLOSE_BRACE, tokens)) {
+        body.push_back(parseOperator(tokens));
     }
+
+    skipToken(TT_OPERATION, CLOSE_BRACE, tokens);
 }
 
-Token getToken(vector<Token> tokens) {
-    Token token = tokens.at(0);
-    tokens.erase(tokens.begin());
+Function* parseFunction(vector<Token> tokens) {
+    Function* function = new Function();
+    int name;
 
-    return token;
+    readFunctionType(tokens);
+    name = readFunctionName(tokens);
+
+    skipToken(TT_OPERATION, OPEN_PARENTHESE, tokens);
+
+    //readParameters(tokens);
+
+    skipToken(TT_OPERATION, CLOSE_PARENTHESE, tokens);
+
+    vector<Operator*> body = parseBody(tokens);
+
+    function->name = name;
+    function->body = body;
+    if (name == -1) function->isMain = true; // main number
+
+    return function;
 }
 
-Token getToken(stack<Token> stack) {
-    Token token = stack.top();
-    stack.pop();
-
-    return token;
+IR parseProgram(vector<Token> tokens) {
+    while (!tokens.empty()) {
+        parseFunction(tokens);
+    }
 }
--- src/parser/Parser.h
+++ src/parser/Parser.h
@@ -7,16 +7,3 @@
 #include "../base.h"
 
 IR parseProgram(std::vector<Token> tokens);
-Function* parseFunction(std::vector<Token> tokens);
-std::vector<Operator*> parseBody(std::vector<Token> tokens);
-std::string readFunctionType(std::vector<Token> tokens);
-int readFunctionName(std::vector<Token> tokens);
-Expression* parseExpression(std::vector<Token> tokens);
-void skipToken(TokenType tokenType, int ttType, std::vector<Token> tokens);
-Operator* parseOperator(std::vector<Token> tokens);
-Token getToken(std::vector<Token> tokens);
-bool nextToken(TokenType tokenType, int ttType, std::vector<Token> tokens);
-bool nextToken(TokenType tokenTyp, std::vector<Token> tokens);
-bool nextToken(TokenType tokenType, int ttType, std::stack<Token> stack);
-Token getToken(std::stack<Token> stack);
-int getPriority(Token token);
